e827bb9465e447b8835aebf40564aa17fa2e8670
yuriEGV <100158638+yuriEGV@users.noreply.github.com>
2025-05-06 10:08:03 -0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const serverless = require('serverless-http');
 const conectarDB = require('../config/db');
 const authRoutes = require('../routes/authRoutes');
@@ -14,3 +14,23 @@ app.use('/api/auth', authRoutes);
 
 module.exports = app;
 module.exports.handler = serverless(app);
+*/
+
+// api/index.js
+import express from 'express';
+import { router } from '../routes/authRoutes.js'; // ajusta la ruta si es necesario
+import { verifyToken } from '../controllers/authController.js';
+import connectDB from '../utils/db.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', router);
+app.use('/api/protected', verifyToken, (req, res) => {
+  res.json({ msg: 'Ruta protegida accedida correctamente' });
+});
+
+export default app;
